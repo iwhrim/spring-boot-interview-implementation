77f08ccf479d9060d1f93e2eb9adeb3084e2e8cd
Gustavo Teixeira <iwhrim@gmail.com>
2020-03-17 11:14:19 -0300

    First commit

---
+++ city/src/test/java/dev/iwrim/city/CityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iwrim.city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
